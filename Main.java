44a88a98a0b5ce0225dc7fb4794c81c47ddf8b6f
Anas <anas.saad.nadir>
2021-12-09 00:18:45 +0100

    just oop

---
+++ Main.java
@@ -0,0 +1,18 @@
+package sandwich;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Sandwich s1 = new Sandwich();
+		s1.price();
+		Sandwich s2 = new Sandwich("tomat");
+		s2.price();
+		Sandwich s3 = new Sandwich("tomat", "onion");
+		s3.price();
+		Sandwich s4 = new Sandwich("tomat", "onion", "suas");
+		s4.price();
+		
+		
+	}
+
+}
---
+++ Sandwich.java
@@ -0,0 +1,43 @@
+package sandwich;
+
+public class Sandwich {
+	String tomat ;
+	String onion  ;
+	String saus ;
+	int tomatPrice = 10 ;
+	int onionPrice = 15 ;
+	int sausPrice = 5 ;
+	int naturPrice = 50;
+	int totalPrice;
+	static int noOfreicept = 0;
+
+	public Sandwich() {
+		totalPrice = naturPrice;
+		noOfreicept++;
+	}
+
+	public Sandwich(String tomat) {
+//		this.tomat = tomat;
+		totalPrice = naturPrice + tomatPrice ;
+		noOfreicept++;
+	}
+
+	public Sandwich(String tomat, String onion) {
+		this.tomat = tomat;
+		this.onion = onion;
+		totalPrice = naturPrice + this.tomatPrice + this.onionPrice;
+		noOfreicept++;
+	}
+
+	public Sandwich(String tomat, String onion, String saus) {
+		this(tomat, onion);
+		this.saus = saus;
+		totalPrice = naturPrice + this.tomatPrice + this.onionPrice + this.sausPrice;
+		noOfreicept++;
+	}
+
+	public void price() {
+		System.out.printf("your no is %d the price is %d\n", noOfreicept, this.totalPrice);
+	}
+
+}
