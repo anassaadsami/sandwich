d5ccdac8d908d60e315f1472eabb36e45b58a2ee
Anas <anas.saad.nadir>
2021-12-09 01:29:59 +0100

    initial commit

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ package sandwich;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		// here we make some objects of class Sandwich
 		Sandwich s1 = new Sandwich();
 		s1.price();
 		Sandwich s2 = new Sandwich("tomat");
